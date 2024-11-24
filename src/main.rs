cefbf99b0714d5ca1df10b6b85ab398cef914a9a
green-ogre <balln13572@gmail.com>
2024-11-23 19:15:06 -0700

    init bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::default()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, bevy_bits::close_on_escape)
+        .run();
 }
